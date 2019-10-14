49a261b2206e602366f9b7433b8f0955413bb76b
Frederik Jørgensen <fjvemmedrup@gmail.com>
2019-10-14 12:51:06 +0200

    moar components

---
+++ screens/AllGiants.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { withNavigation } from "react-navigation";
+
+export default class AllGiants extends React.Component {
+  render() {
+    return <Text>All Giants</Text>;
+  }
+}
