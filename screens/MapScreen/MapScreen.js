29e3c09e46a2551be100fd8f5ce10ccee7224ec9
cami <camha@itu.dk>
2019-11-11 10:20:44 +0100

    map - user location

--- screens/MapScreen/MapScreen.js
+++ screens/MapScreen/MapScreen.js
@@ -10,12 +10,60 @@ import MapView from "react-native-maps";
 import { OrangeButton } from "../../components/Buttons/OrangeButton";
 
 export default class MapScreen extends React.Component {
+  state = {
+
+  };
+
+  async getCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(
+        position => {
+        let region = {
+                latitude: parseFloat(position.coords.latitude),
+                longitude: parseFloat(position.coords.longitude),
+                latitudeDelta: 0.0322,
+                longitudeDelta: 0.0221
+            };
+
+        this.setState({
+            initialRegion: region
+          });
+        },
+
+        error => console.log(error),
+        {
+            enableHighAccuracy: true,
+            timeout: 20000,
+            maximumAge: 1000
+        }
+    );
+}
+
+componentDidMount(){
+ this.getCurrentLocation();
+}
+
+goToInitialLocation() {
+    let initialRegion = Object.assign({}, this.state.initialRegion);
+    initialRegion["latitudeDelta"] = 0.005;
+    initialRegion["longitudeDelta"] = 0.005;
+    this.mapView.animateToRegion(initialRegion, 2000);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <MapView style={styles.mapStyle} />
+        <MapView
+          style={styles.mapStyle}
+          region={this.state.region}
+          followUserLocation={true}
+          ref={ref => (this.mapView = ref)}
+          zoomEnabled={true}
+          showsUserLocation={true}
+          onMapReady={this.goToInitialLocation.bind(this)}
+          initialRegion={this.state.initialRegion}
+        />
         <View style={styles.bottom}>
-          <Text>NEAREST TRAIN STATION: Ishøj st. </Text>
+          <Text style={styles.textStyle}> Public transport: Ishøj st. </Text>
           <OrangeButton
             btnText='Next >'
             onPress={() => this.props.navigation.navigate("RewardScreen")}>
@@ -39,9 +87,13 @@ const styles = StyleSheet.create({
     flex: 1,
     marginLeft: 7
   },
+  textStyle: {
+    fontFamily: "amatic-sc",
+    fontSize: 35,
+    marginTop: 5
+  },
   mapStyle: {
     width: Dimensions.get("window").width,
-    height: 100,
-    flex: 2
+    flex: 3
   }
 });
