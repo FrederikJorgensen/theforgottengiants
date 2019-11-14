53693c2a7fce06518133e6b730fe320c3bc17a2c
Frederik Jørgensen <fjvemmedrup@gmail.com>
2019-11-14 13:39:51 +0100

    added giants.js file

---
+++ data/giants.js
@@ -0,0 +1,56 @@
+export default [
+  {
+    id: 1,
+    name: "Teddy",
+    location: "HØJE TAASTRUP",
+    image: require("../assets/images/Teddy.png"),
+    adress: "Hakkemosevej, 2630 Taastrup",
+    transport: "Høje Taastrup st., bus 167",
+    region: {
+      latitude: 55.661838,
+      longitude: 12.266058,
+      latitudeDelta: 0.0122,
+      longitudeDelta: 0.0121
+    }
+  },
+  {
+    id: 2,
+    name: "Oscar Under The Bridge",
+    location: "Ishøj",
+    image: require("../assets/images/Oscar.png"),
+    adress: "Hakkemosevej, 2630 Taastrup",
+    transport: "Høje Taastrup st."
+  },
+  {
+    id: 3,
+    name: "Sleeping Louis",
+    location: "Glostrup",
+    image: require("../assets/images/Louis.png"),
+    adress: "Hakkemosevej, 2630 Taastrup",
+    transport: "Høje Taastrup st."
+  },
+  {
+    id: 4,
+    name: "Little Tilde",
+    location: "Vallensbæk",
+    image: require("../assets/images/Tilde.png"),
+    adress: "Hakkemosevej, 2630 Taastrup",
+    transport: "Høje Taastrup st."
+  },
+  {
+    id: 5,
+    name: "Thomas on The Mountain",
+    location: "Albertslund",
+    image: require("../assets/images/Thomas.png"),
+    adress: "Hakkemosevej, 2630 Taastrup",
+    transport: "Høje Taastrup st."
+  },
+  {
+    id: 6,
+    name: "Trine",
+    location: "Avedøre",
+    image: require("../assets/images/Trine.png"),
+    adress: "Hakkemosevej, 2630 Taastrup",
+    transport: "Høje Taastrup st."
+  }
+];
--- screens/AllGiantsScreen/AllGiantsScreen.js
+++ screens/AllGiantsScreen/AllGiantsScreen.js
@@ -4,47 +4,48 @@ import Giant from "../../components/Giant/Giant";
 import styles from "./AllGiantsStyles";
 console.disableYellowBox = true;
 import * as Font from "expo-font";
+import giants from "../../data/giants";
 
-export default class AllGiantsScreen extends React.Component {
+class AllGiantsScreen extends React.Component {
   state = {
-    giants: [
-      {
-        id: 1,
-        name: "Teddy",
-        location: "HØJE TAASTRUP",
-        image: require("../../assets/images/Teddy.png")
-      },
-      {
-        id: 2,
-        name: "Oscar Under The Bridge",
-        location: "Ishøj",
-        image: require("../../assets/images/Oscar.png")
-      },
-      {
-        id: 3,
-        name: "Sleeping Louis",
-        location: "Glostrup",
-        image: require("../../assets/images/Louis.png")
-      },
-      {
-        id: 4,
-        name: "Little Tilde",
-        location: "Vallensbæk",
-        image: require("../../assets/images/Tilde.png")
-      },
-      {
-        id: 5,
-        name: "Thomas on The Mountain",
-        location: "Albertslund",
-        image: require("../../assets/images/Thomas.png")
-      },
-      {
-        id: 6,
-        name: "Trine",
-        location: "Avedøre",
-        image: require("../../assets/images/Trine.png")
-      }
-    ]
+    // giants: [
+    //   {
+    //     id: 1,
+    //     name: "Teddy",
+    //     location: "HØJE TAASTRUP",
+    //     image: require("../../assets/images/Teddy.png")
+    //   },
+    //   {
+    //     id: 2,
+    //     name: "Oscar Under The Bridge",
+    //     location: "Ishøj",
+    //     image: require("../../assets/images/Oscar.png")
+    //   },
+    //   {
+    //     id: 3,
+    //     name: "Sleeping Louis",
+    //     location: "Glostrup",
+    //     image: require("../../assets/images/Louis.png")
+    //   },
+    //   {
+    //     id: 4,
+    //     name: "Little Tilde",
+    //     location: "Vallensbæk",
+    //     image: require("../../assets/images/Tilde.png")
+    //   },
+    //   {
+    //     id: 5,
+    //     name: "Thomas on The Mountain",
+    //     location: "Albertslund",
+    //     image: require("../../assets/images/Thomas.png")
+    //   },
+    //   {
+    //     id: 6,
+    //     name: "Trine",
+    //     location: "Avedøre",
+    //     image: require("../../assets/images/Trine.png")
+    //   }
+    // ]
   };
 
   async componentDidMount() {
@@ -56,25 +57,28 @@ export default class AllGiantsScreen extends React.Component {
   }
 
   render() {
-    const { giants } = this.state;
+    console.log(giants);
     return (
       <View style={styles.container}>
         <ScrollView>
           <Text style={styles.giantText}>GO FIND A GIANT</Text>
           <TouchableOpacity
-          onPress={() => this.props.navigation.navigate("MapScreen")}>
-          {this.state.giants.map(giant => (
-            <Giant
-              style={styles.giant}
-              name={giant.name}
-              location={giant.location}
-              image={giant.image}
-              key={giant.id}
+            onPress={() => this.props.navigation.navigate("MapScreen")}
+          >
+            {giants.map(giant => (
+              <Giant
+                style={styles.giant}
+                name={giant.name}
+                location={giant.location}
+                image={giant.image}
+                key={giant.id}
               />
-          ))}
+            ))}
           </TouchableOpacity>
         </ScrollView>
       </View>
     );
   }
 }
+
+export default AllGiantsScreen;
