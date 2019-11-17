1f3138021a6b693d14197e54f70a8c55533cd3b8
camfle <camillafleron87@gmail.com>
2019-11-17 13:57:03 +0100

    styling adjusted

--- screens/RewardCollection/RewardCollectionStyles.js
+++ screens/RewardCollection/RewardCollectionStyles.js
@@ -1,21 +1,26 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+  containerScroll:{
+    marginBottom: 45,
+    flexDirection: "column"
+  },
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#48972C",
-    
+
   },
   rewardTitle: {
     fontFamily: "amatic-sc",
-    fontSize: 60,
+    fontSize: 50,
     color: "black",
-    marginTop: "15%",
-    marginBottom: 40,
+    marginTop: "5%",
+    marginBottom: 20,
     textAlign: "center"
   }
 });
 
 export default styles;
+
