fa495566155e5922d729d75261e5afc094c68169
Natascha Jensen <nhoegsted@live.dk>
2019-11-17 20:05:56 +0100

    Added details
    Added more correct data for addresses

--- data/giants.js
+++ data/giants.js
@@ -2,7 +2,7 @@ export default [
   {
     id: 1,
     name: "Teddy Friendly",
-    location: "HØJE TAASTRUP",
+    location: "Høje Taastrup",
     image: require("../assets/images/Teddy.png"),
     desc:
       "Teddy Friendly is a recycle sculpture Thomas Dambo made as a part of the larger project “Forgotten Giants”. The project consists of 6 large sculptures made in the outskirts of Copenhagen. Common for all sculptures is that they are made solely from local scrapwood and recycled materials, and made in a hidden beautiful location. This way it invites the viewers to go on a treasure hunt, not only to see the sculptures, but also to discover hidden gems in nature.\n\nTeddy friendly is made from local scrapwood, including cut offs from cut down local trees, which has been used for his fur. He is standing at a water stream, at a lake holding out his hand helping people to cross.\n\nThe Thomas Dambo team had help from a local activation center, which provided four unemployed people and a teacher to assist in building the sculpture. The teachers name was Teddy, he was a super friendly and hardworking guy, and together with his team he made a huge effort, therefore Thomas Dambo decided to name the sculpture after him. ",
@@ -22,8 +22,8 @@ export default [
     image: require("../assets/images/Oscar.png"),
     desc:
       "Oscar Under The Bridge is a recycle sculpture Thomas Dambo made as a part of the larger project “Forgotten Giants”. The project consists of 6 large sculptures made in the outskirts of Copenhagen. Common for all sculptures is that they are made solely from local scrapwood and recycled materials, and made in a hidden beautiful location. This way it invites the viewers to go on a treasure hunt, not only to see the sculptures, but also to discover hidden gems in nature.\n\nOscar under the bridge is made from scrapwood from a torn down watermill and broken pallets from local industries. He is named after an artist from Chile who came to visit Thomas and help him in his work during the time that Thomas and his team was building the sculpture.",
-    address: "Hakkemosevej, 2630 Taastrup",
-    transport: "Høje Taastrup st.",
+    address: "Jægersø, 2635 Ishøj",
+    transport: "Ishøj st., bus 128",
     region: {
       latitude: 55.601398,
       longitude: 12.372434,
@@ -34,12 +34,12 @@ export default [
   {
     id: 3,
     name: "Sleeping Louis",
-    location: "Glostrup",
+    location: "Rødovre",
     image: require("../assets/images/Louis.png"),
     desc:
       "Sleeping Louis is a recycle sculpture Thomas Dambo made as a part of the larger project “Forgotten Giants”. The project consists of 6 large sculptures made in the outskirts of Copenhagen. Common for all sculptures is that they are made solely from local scrapwood and recycled materials, and made in a hidden beautiful location. This way it invites the viewers to go on a treasure hunt, not only to see the sculptures, but also to discover hidden gems in nature.\n\nSleeping Louis is taking a nap on a hill, covered in trees and nature, in a secret place in Rødovre outside of Copenhagen. People can crawl into his gaping mouth, and play or even sleep inside of him.\n\nSleeping Louis is made solely from local scrapwood scavenged by the Thomas Dambo team. To build the sculpture the team had help from a group of young volunteers from an organisation helping young unemployed people get work experience before reentering the work market. Thomas’ old assistant Louis from Bristol, England came back to Copenhagen for a visit, and worked hard to help finish the sculpture, why Thomas and the team decided to name the sculpture after him.",
-    address: "Hakkemosevej, 2630 Taastrup",
-    transport: "Høje Taastrup st.",
+    address: "Korsdalsvej, 2610 Rødovre",
+    transport: "bus 9A",
     region: {
       latitude: 55.674023,
       longitude: 12.431133,
@@ -54,8 +54,8 @@ export default [
     image: require("../assets/images/Tilde.png"),
     desc:
       "Little Tilde is a recycle sculpture Thomas Dambo made as a part of the larger project “Forgotten Giants”. The project consists of 6 large sculptures made in the outskirts of Copenhagen. Common for all sculptures is that they are made solely from local scrapwood and recycled materials, and made in a hidden beautiful location. This way it invites the viewers to go on a treasure hunt, not only to see the sculptures, but also to discover hidden gems in nature.\n\nLittle Tilde is located in the area of Vallensbæk Mose, af beautiful area filled with wild nature and animal life. She watches through the trees at the other side of a small lake, from where you will be able to catch a glimpse of her at a distance.\n\nTilde is made solely from local scrapwood scavenged by the Thomas Dambo team. To build the sculpture the team had help from a group of volunteers including two senior craftsmen (one of which was aged 86), and a couple of locals including Tilde, from whom the sculpture got her name.\n\nAs a little bonus Thomas Dambo made 28 birdhouses inside the sculpture, for the birds and maybe a squirrel to seek shelter when winter hits Vallensbæk Mose.",
-    address: "Hakkemosevej, 2630 Taastrup",
-    transport: "Høje Taastrup st.",
+    address: "Tueholmsøen, 2625 Vallensbæk",
+    transport: "Vallensbæk st., Bus 143, bus 144",
     region: {
       latitude: 55.643548,
       longitude: 12.345511,
@@ -70,8 +70,8 @@ export default [
     image: require("../assets/images/Thomas.png"),
     desc:
       "Thomas On The Mountain is a recycle sculpture Thomas Dambo made as a part of the larger project “Forgotten Giants”. The project consists of 6 large sculptures made in the outskirts of Copenhagen. Common for all sculptures is that they are made solely from local scrapwood and recycled materials, and made in a hidden beautiful location. This way it invites the viewers to go on a treasure hunt, not only to see the sculptures, but also to discover hidden gems in nature.\n\nThomas On The Mountain has found a good place to chill on top of a hill. From here he has a great view of the municipality of Albertslund, and so can you if you join him. His legs are long, and a good place to sit with your friends.\n\nThe giant sculpture is made solely from local scrapwood scavenged by the Thomas Dambo team. To build the sculpture the team had help from a group of young volunteers from a local school as well as a couple of seniors. The giant is named after Thomas Dambos intern also named Thomas, who has been a huge part of the Forgotten Giants project, and helped create all six sculptures.",
-    address: "Hakkemosevej, 2630 Taastrup",
-    transport: "Høje Taastrup st.",
+    address: "Kongsholmbjerget, 2620 Albertslund",
+    transport: "Vallensbæk st., Bus 143, bus 144",
     region: {
       latitude: 55.647515,
       longitude: 12.342947,
@@ -86,8 +86,8 @@ export default [
     image: require("../assets/images/Trine.png"),
     desc:
       "Hill Top Trine is a recycle sculpture Thomas Dambo made as a part of the larger project “Forgotten Giants”. The project consists of 6 large sculptures made in the outskirts of Copenhagen. Common for all sculptures is that they are made solely from local scrapwood and recycled materials, and made in a hidden beautiful location. This way it invites the viewers to go on a treasure hunt, not only to see the sculptures, but also to discover hidden gems in nature.\n\nHill Top Trine is resting on top of a little hill in Hvidovre, Copenhagen, where you can crawl into the palms of her hands and get a beautiful viewpoint overlooking Avedøresletten. She is named after one of the volunteers who worked on one of the other hidden giants.\n\nJust like her siblings, Hill Top Trine is made from scrapwood, branches and old pallets from Stark. The Thomas Dambo team have had help building the wooden sculpture from a handful of local volunteers.",
-    address: "Hakkemosevej, 2630 Taastrup",
-    transport: "Høje Taastrup st.",
+    address: "Parkeringsplads ved volden, 2650 Hvidovre",
+    transport: "Bus 1A",
     region: {
       latitude: 55.633884,
       longitude: 12.441283,
