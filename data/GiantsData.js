ff8aabb5842309e215f32394988a9db36780e974
Natascha Jensen <nhoegsted@live.dk>
2019-11-29 16:33:08 +0100

    Added audio file and link to it in GiantsData

--- data/GiantsData.js
+++ data/GiantsData.js
@@ -5,6 +5,7 @@ export default [
     firstname: "Teddy",
     location: "Høje Taastrup",
     image: require("../assets/images/Teddy.png"),
+    audio: require("../assets/sounds/Teddy.mp3"),
     desc:
       'Teddy Friendly is a recycle sculpture Thomas Dambo made as a part of the larger project "Forgotten Giants". The project consists of 6 large sculptures made in the outskirts of Copenhagen. Common for all sculptures is that they are made solely from local scrapwood and recycled materials, and made in a hidden beautiful location. This way it invites the viewers to go on a treasure hunt, not only to see the sculptures, but also to discover hidden gems in nature.\n\nTeddy friendly is made from local scrapwood, including cut offs from cut down local trees, which has been used for his fur. He is standing at a water stream, at a lake holding out his hand helping people to cross.\n\nThe Thomas Dambo team had help from a local activation center, which provided four unemployed people and a teacher to assist in building the sculpture. The teachers name was Teddy, he was a super friendly and hardworking guy, and together with his team he made a huge effort, therefore Thomas Dambo decided to name the sculpture after him.',
     region: {
